6b3d069ba7a0263fb5388db54b426d2cf598e371
Sulphur <robuxtrex@gmail.com>
2022-12-17 14:41:43 +0000

    Basic website with 2 docs pages added.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{mdx,tsx,jsx,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+    logo: <span>Quacker Discord Bot Documentation</span>,
+    project: {
+    link: 'https://github.com/robuxtrex/quacker',
+    },
+    chat: {
+        link: 'https://discord.gg/Qb5yzH56Jz'
+    },
+    banner: {
+        key: 'Quacker-Incomplete',
+        text: <a>This site is a proof of concept! Quacker is still highly in development.</a>,
+        dismissible: false,
+    },
+}
+   
